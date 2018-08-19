0fb52f0d5148f5001adff9b1cb7f1870b1511d26
Krishna Sagar R <ragas.sagar.474@gmail.com>
2018-08-19 12:08:38 +0100

    feat: add records reducer

--- src/reducers/records.js
+++ src/reducers/records.js
@@ -1,99 +1,91 @@
-import { createRecordsReducer, __toRecords } from "./";
+export const __toRecords = (__records = []) => {
+  if (!Array.isArray(__records))
+    return {
+      records: {},
+      ids: []
+    };
+  const records = __records.reduce((map, obj) => {
+    map[obj.id] = obj;
+    return map;
+  }, {});
+  return {
+    records,
+    ids: Object.keys(records)
+  };
+};
 
-describe("Records reducer", () => {
-  describe("Adding records", () => {
-    const reducer = createRecordsReducer({
-      namespace: "SOME"
-    });
+export const __updateAllRecords = (state, action) => __toRecords(action.data);
 
-    it("can add a single record", () => {
-      expect(reducer({}, { type: "" })).toEqual({});
-      expect(reducer({}, { type: "SOME/ADDED" })).toEqual({});
-      expect(
-        reducer(undefined, { type: "SOME/ADDED", data: { id: 1 } })
-      ).toEqual({ ids: [1], records: { "1": { id: 1 } } });
-      expect(
-        reducer(__toRecords([{ id: 4 }, { id: 5 }]), {
-          type: "SOME/ADDED",
-          data: { id: 1 }
-        })
-      ).toEqual({
-        ids: ["4", "5", 1],
-        records: {
-          "1": { id: 1 },
-          "4": { id: 4 },
-          "5": { id: 5 }
-        }
-      });
-    });
+export const __updateRecords = (state, action) => {
+  if (!action.data || !state.records[action.data.id]) {
+    return state;
+  }
+  if (!Array.isArray(action.data) && state.records[action.data.id]) {
+    return {
+      records: { ...state.records, [action.data.id]: action.data },
+      ids: state.ids
+    };
+  } else if (Array.isArray(action.data) && state.records[action.data.id]) {
+    return state;
+  }
+  return state;
+};
 
-    it("can add multiple records", () => {
-      expect(reducer({}, { type: "" })).toEqual({});
-      expect(reducer({}, { type: "SOME/ADDED" })).toEqual({});
-      expect(
-        reducer(undefined, { type: "SOME/ADDED", data: [{ id: 1 }, { id: 2 }] })
-      ).toEqual({
-        ids: ["1", "2"],
-        records: { "1": { id: 1 }, "2": { id: 2 } }
-      });
-    });
-  });
+export const __addRecords = (state, action) => {
+  if (!action.data) return state;
+  if (!Array.isArray(action.data)) {
+    return {
+      records: { ...state.records, [action.data.id]: action.data },
+      ids: [...state.ids, action.data.id]
+    };
+  }
+  return {
+    records: { ...state.records, ...__toRecords(action.data).records },
+    ids: [...state.ids, ...__toRecords(action.data).ids]
+  };
+};
 
-  describe("Updateing records", () => {
-    const reducer = createRecordsReducer({
-      namespace: "SOME"
-    });
+export const __removeRecords = (state, action) => {
+  const __state = Object.assign({}, state);
+  if (!Array.isArray(action.data)) {
+    delete __state.records[action.data.id];
+    return {
+      records: __state.records,
+      ids: state.ids
+    };
+  }
+  return {
+    records: { ...state.records, ...__toRecords(action.data) },
+    ids: state.ids
+  };
+};
 
-    it("can update a single record", () => {
-      expect(reducer({}, { type: "" })).toEqual({});
-      expect(reducer({}, { type: "SOME/UPDATED" })).toEqual({});
-      expect(
-        reducer(undefined, { type: "SOME/UPDATED", data: { id: 1 } })
-      ).toEqual({ ids: [], records: {} });
-      expect(
-        reducer(__toRecords([{ id: 1 }, { id: 2 }]), {
-          type: "SOME/UPDATED",
-          data: { id: 1, name: "some name" }
-        })
-      ).toEqual(__toRecords([{ id: 1, name: "some name" }, { id: 2 }]));
-    });
+export const createRecordsReducer = ({ namespace }) => {
+  return (
+    state = {
+      records: {},
+      ids: []
+    },
+    action
+  ) => {
+    if (new RegExp(`${namespace}/CHANGED`).test(action.type)) {
+      return __updateAllRecords(state, action);
+    } else if (new RegExp(`${namespace}/UPDATED`).test(action.type)) {
+      return __updateRecords(state, action);
+    } else if (new RegExp(`${namespace}/ADDED`).test(action.type)) {
+      return __addRecords(state, action);
+    } else if (new RegExp(`${namespace}/REMOVED`).test(action.type)) {
+      return __removeRecords(state, action);
+    }
+    return state;
+  };
+};
 
-    it("can update multiple records", () => {
-      expect(reducer({}, { type: "" })).toEqual({});
-      expect(reducer({}, { type: "SOME/ADDED" })).toEqual({});
-      expect(
-        reducer(undefined, { type: "SOME/ADDED", data: [{ id: 1 }, { id: 2 }] })
-      ).toEqual({
-        ids: ["1", "2"],
-        records: { "1": { id: 1 }, "2": { id: 2 } }
-      });
-    });
-  });
-
-  describe("Changeing records", () => {
-    const reducer = createRecordsReducer({
-      namespace: "SOME"
-    });
-
-    it("can update a single record", () => {
-      expect(reducer({}, { type: "" })).toEqual({});
-      expect(reducer(__toRecords([]), { type: "SOME/CHANGED" })).toEqual(
-        __toRecords([])
-      );
-      expect(
-        reducer(__toRecords([]), { type: "SOME/CHANGED", data: [{ id: 1 }] })
-      ).toEqual(__toRecords([{ id: 1 }]));
-    });
-
-    it("can update multiple records", () => {
-      expect(reducer({}, { type: "" })).toEqual({});
-      expect(reducer({}, { type: "SOME/ADDED" })).toEqual({});
-      expect(
-        reducer(undefined, { type: "SOME/ADDED", data: [{ id: 1 }, { id: 2 }] })
-      ).toEqual({
-        ids: ["1", "2"],
-        records: { "1": { id: 1 }, "2": { id: 2 } }
-      });
-    });
-  });
-});
+export const loadable = namespace => reducer => {
+  return (state, actoin) => {
+    return {
+      ...reducer(state, actoin),
+      loading: new RegExp(`${namespace}/LOADING`).test(actoin.type)
+    };
+  };
+};
